c231e82b7adcbde041a373d8eb69b1b89ef10102
GeovannyTS <yowtecum@gmail.com>
2023-09-19 16:41:31 -0600

    Implementacion de sidebar

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface MenuItem{
+  name: string;
+  route: string;
+}
+
 @Component({
-  selector: 'app-side-menu',
+  selector: 'side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
 
+  public menuItem: MenuItem[] = [
+    {route: '/maps/fullscreen', name: 'FullScreen'},
+    {route: '/maps/zoom-range', name: 'ZoomRange'},
+    {route: '/maps/markers', name: 'Markers'},
+    {route: '/maps/properties', name: 'Properties'},
+  ]
 }
